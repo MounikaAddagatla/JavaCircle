8ed0cb7706fcaad4621ec029fb958e12f9604097
mounika <mounaadagatla@gmail.com>
2021-09-15 21:35:59 +0530

    refactored - added code for second line length

--- LineLength.java
+++ LineLength.java
@@ -4,6 +4,10 @@ class Circle{
 		int x1=5, x2=13;
 		int y1=3, y2=10;
 		int length = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
-		System.out.println(length)
+		System.out.println(length);
+		int x1_1=2, x2_1=1;
+		int y1_1=3, y2_1=-19;
+		int length_1 = Math.sqrt((x2_1-x1_1)*(x2_1-x1_1)+(y2_1-y1_1)*(y2_1-y1_1));
+		System.out.println(length_1);
 	}
 }
