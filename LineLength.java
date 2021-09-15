77c12b692d96784a9637381b666e6044c7ebf0fb
mounika <mounaadagatla@gmail.com>
2021-09-15 21:15:31 +0530

    added code to fine line length

---
+++ LineLength.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+class Circle{
+	public static void main(String args[]){
+		int x1=5, x2=13;
+		int y1=3, y2=10;
+		int length = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+		System.out.println(length)
+	}
+}
